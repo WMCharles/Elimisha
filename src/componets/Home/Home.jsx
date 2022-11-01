a69d42aa082a0ae235b5c93ed09d6469dfc8c5c0
WMCharles <charles.wafula@student.moringaschool.com>
2022-11-01 20:46:42 +0300

    add Testimonials component

--- src/componets/Home/Home.jsx
+++ src/componets/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import '../../styles/Home/Home.css'
 import Cards from './Cards'
 import Hero from './Hero'
+import Testimonials from './Testimonials'
 
 export default function Home() {
   return (
     <div className='container'>
       <Hero/>
       <Cards/>
+      <Testimonials/>
     </div>
   )
 }
