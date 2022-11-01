cca6401f49f2b7a2023bb569200b949d1a57c318
WMCharles <charles.wafula@student.moringaschool.com>
2022-11-01 20:47:03 +0300

    add testimonals content

--- src/componets/Home/Testimonials.jsx
+++ src/componets/Home/Testimonials.jsx
@@ -1,7 +1,55 @@
 import React from 'react'
-
+import '../../styles/Home/Testimonials.css'
 export default function Testimonials() {
-  return (
-    <div>Testimonials</div>
-  )
+    return (
+      <div className='testimonials'>
+        <h1>Testimonials</h1>
+        <h2>What Our Students Say</h2>
+        <div className="feedback-session">
+          <div className="feedback-container">
+            <div className='feedback'>
+                <div>
+                  Calvin: Sometimes when I'm talking with others, my words can't keep up with my thoughts. I wonder why we think faster than we speak. Hobbes: Probably so we can think twice.
+                </div>
+                <div className="author">
+                  <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sq-sample1.jpg" alt="author" />
+                  <h5>Pelican Steve <span> LittleSnippets</span></h5>
+                </div> 
+            </div>
+          </div>    
+          <div className="feedback-container">
+            <div className='feedback'>
+                <div>
+                  Calvin: Sometimes when I'm talking with others, my words can't keep up with my thoughts. I wonder why we think faster than we speak. Hobbes: Probably so we can think twice.
+                </div>
+                <div className="author">
+                  <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sq-sample1.jpg" alt="author" />
+                  <h5>Pelican Steve <span> LittleSnippets</span></h5>
+                </div> 
+            </div>
+          </div>    
+          <div className="feedback-container">
+            <div className='feedback'>
+                <div>
+                  Calvin: Sometimes when I'm talking with others, my words can't keep up with my thoughts. I wonder why we think faster than we speak. Hobbes: Probably so we can think twice.
+                </div>
+                <div className="author">
+                  <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sq-sample1.jpg" alt="author" />
+                  <h5>Pelican Steve <span> LittleSnippets</span></h5>
+                </div> 
+            </div>
+          </div>    <div className="feedback-container">
+            <div className='feedback'>
+                <div>
+                  Calvin: Sometimes when I'm talking with others, my words can't keep up with my thoughts. I wonder why we think faster than we speak. Hobbes: Probably so we can think twice.
+                </div>
+                <div className="author">
+                  <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sq-sample1.jpg" alt="author" />
+                  <h5>Pelican Steve <span> LittleSnippets</span></h5>
+                </div> 
+            </div>
+          </div>         
+        </div>
+      </div>
+    )
 }
